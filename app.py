485d88b54027b9f72ca50ebceddee7989ac149ef
stutiuneyal <100413549+stutiuneyal@users.noreply.github.com>
2024-07-30 22:05:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pytesseract
 import re
 import os
 #pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/"
 
 
 app = Flask(__name__)
